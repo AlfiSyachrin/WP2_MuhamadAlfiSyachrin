495a277fed63d4a6b0d2ed2f1daaa0254944e874
AlfiSyachrin <alfisyachrin07@gmail.com>
2023-09-26 20:11:29 +0700

    menambahkan folder/file pusataka.booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extend CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Alfi Syachrin
+            saya tinggal di daerah Tokyo Jepang
+            saya suka makan Ramen campur Nasi";
+    }
+}
