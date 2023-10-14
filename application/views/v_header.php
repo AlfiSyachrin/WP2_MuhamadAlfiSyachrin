64b147ee28ad010dffb85d6332251ecf7a2b468b
AlfiSyachrin <alfisyachrin07@gmail.com>
2023-10-14 14:37:50 +0700

    memodifikasi file matakuliah.php tugas pertemuan4-5

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -4,9 +4,9 @@
     <meta charset=”utf-8”>
     <title>Web Prog II | Merancang Template sederhana dengan 
 codeigniter</title>
-    <link rel="stylesheet" type=”text/css” href=”<?php echo 
-base_url() ?>assets/css/stylebuku.css”>
 </head>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() 
+?>assets/css/stylebuku.css"
 <body>
     <div id="wrapper">
         <header>
@@ -17,8 +17,7 @@ CodeIgniter</h3>
             </hgroup>
             <nav>
                 <ul>
-                <li><a href="<?php echo 
-base_url().'index.php/web' ?>">Home</a></li>
+                <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li>
                 <li><a href="<?php echo 
 base_url().'index.php/web/about' ?>">About</a></li>
             </ul>
